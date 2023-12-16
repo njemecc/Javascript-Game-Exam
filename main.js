11caf353a577e93a75fc1eec156c86915302ce6d
brani <branislannjemec@gmail.com>
2023-12-16 12:42:17 +0100

    addin single pictureframe

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Player } from "./player";
+
 window.addEventListener("load", function () {
   const canvas = this.document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
@@ -8,10 +10,24 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.player = new Player(this);
     }
 
     update() {}
 
-    draw() {}
+    draw(context) {
+      this.player.draw(context);
+    }
   }
+
+  const game = new Game(canvas.width, canvas.height);
+
+  console.log(game);
+
+  function animate() {
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 });
--- player.js
+++ player.js
@@ -1,5 +1,26 @@
-class Player {
+export class Player {
   constructor(game) {
     this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = 0;
+    this.y = this.game.height - this.height;
+    this.image = document.getElementById("player");
+  }
+
+  update() {}
+
+  draw(context) {
+    context.drawImage(
+      this.image,
+      0,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
