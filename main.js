721d8fc5887727db680ae5500dd1aa3a218dd6d5
brani <branislannjemec@gmail.com>
2023-12-17 11:01:31 +0100

    enemies functionality

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { InputHandler } from "./input.js";
 import { Player } from "./player.js";
+import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js";
 
 window.addEventListener("load", function () {
   const canvas = this.document.getElementById("canvas1");
@@ -11,17 +13,47 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.speed = 0;
+      this.maxSpeed = 3;
+      this.background = new Background(this);
       this.groundMargin = 50;
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
 
     update(deltaTime) {
+      this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      //handle Enemies
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.map((enemy) => {
+        enemy.update(deltaTime);
+        if (enemy.markedForDeletion)
+          this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      });
     }
 
     draw(context) {
+      this.background.draw(context);
       this.player.draw(context);
+      this.enemies.map((enemy) => {
+        enemy.draw(context);
+      });
+    }
+    addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5)
+        this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+
+      this.enemies.push(new FlyingEnemy(this));
     }
   }
 
@@ -39,5 +71,5 @@ window.addEventListener("load", function () {
     requestAnimationFrame(animate);
   }
 
-  animate();
+  animate(0);
 });
